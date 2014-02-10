f6b87d485aa16dc8b640956c0dc06927939f87fe
Karmveer Sharma <karmveer@tripfactory.com>
2014-02-10 11:52:36 +0530

    image trial

--- FileUploadPracticeBean.java
+++ FileUploadPracticeBean.java
@@ -1,6 +1,5 @@
 package com.eos.b2c.beans;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
@@ -10,7 +9,6 @@ import java.util.List;
 import com.eos.accounts.data.User;
 import com.eos.b2c.secondary.database.dao.hibernate.SecondaryDBHibernateDAOFactory;
 import com.eos.b2c.secondary.database.model.Destination;
-import com.eos.b2c.content.DestinationContentManager;
 import com.eos.b2c.data.LocationData;
 import com.eos.b2c.secondary.database.model.ContentFile;
 import com.eos.b2c.util.SystemProperties;
@@ -27,34 +25,34 @@ public class FileUploadPracticeBean {
         List<Destination> destIds = SecondaryDBHibernateDAOFactory.getDestinationDAO().findAll();
         for (Destination dest : destIds) {
             String mainImage = dest.getMainImage();
-            if(!mainImage.equals("")) {
-            	System.out.println("image:" + mainImage);
-				InputStream inputStream = null;
-				if(mainImage.contains("tripfactory.com") && mainImage.startsWith("/static")) {
-			    	inputStream = new FileInputStream(mainImage);
-			    } else {
-			    	URL url = new URL(mainImage);
-			        URLConnection connection = url.openConnection();
-			        inputStream = connection.getInputStream();
-			    }
-	
-				FileDataType[] dataTypes = FileSizeGroupType.RECT_2_1
-						.getFileDataTypes();
-				boolean publish = true;
-				User creatorUser = null;
-				Long oldPicId = null;
-				ContentFileCategoryType fileCategoryType = ContentFileCategoryType.DESTINATION;
-				String imageName = "";
-				ContentFile contentFile = FileUploadBean.createContentImageFile(null, inputStream, "me.jpg",
-						"jpg", Arrays.asList(dataTypes), fileCategoryType,
-						imageName, creatorUser, publish, FileDataType.NORMAL,
-						oldPicId, false);
-				String newLocation = contentFile.getFileSystemLocation();
-				System.out.println("location :" + newLocation);
-				dest.setMainImage(newLocation);
-				SecondaryDBHibernateDAOFactory.getDestinationDAO().update(dest);
-	            DAOUtil.commitAll();
-            }
+        	System.out.println("image:" + mainImage);
+			InputStream inputStream = null;
+			if(mainImage.contains("tripfactory.com") && mainImage.startsWith("/static")) {
+				continue;
+			} else if(mainImage.startsWith("http")){
+		    	URL url = new URL(mainImage);
+		        URLConnection connection = url.openConnection();
+		        inputStream = connection.getInputStream();
+		    } else {
+		    	continue;
+		    }
+
+			FileDataType[] dataTypes = FileSizeGroupType.RECT_2_1
+					.getFileDataTypes();
+			boolean publish = true;
+			User creatorUser = null;
+			Long oldPicId = null;
+			ContentFileCategoryType fileCategoryType = ContentFileCategoryType.DESTINATION;
+			String imageName = "";
+			ContentFile contentFile = FileUploadBean.createContentImageFile(null, inputStream, "",
+					"jpg", Arrays.asList(dataTypes), fileCategoryType,
+					imageName, creatorUser, publish, FileDataType.NORMAL,
+					oldPicId, false);
+			String newLocation = contentFile.getFileSystemLocation();
+			System.out.println("location :" + newLocation);
+			dest.setMainImage(newLocation);
+			SecondaryDBHibernateDAOFactory.getDestinationDAO().update(dest);
+            DAOUtil.commitAll();
         }
     }
 	
